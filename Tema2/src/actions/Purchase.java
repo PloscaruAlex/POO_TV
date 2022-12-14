24b085a683e97fa53d6454f72a00ae0b8313fdaa
PloscaruAlex <ploscarualexandru25@gmail.com>
2022-12-14 23:23:54 +0200

    Added functionality for purchase and watch actions.

--- Tema2/src/actions/Purchase.java
+++ Tema2/src/actions/Purchase.java
@@ -1,6 +1,12 @@
 package actions;
 
 import functionality.CurrentSession;
+import functionality.Movie;
+import functionality.OutputHelper;
+import functionality.User;
+import pages.PageFactory;
+
+import java.util.ArrayList;
 
 public class Purchase extends Action {
     public Purchase(CurrentSession session) {
@@ -8,6 +14,75 @@ public class Purchase extends Action {
     }
 
     public void doAction() {
+        ArrayList<String> allowedActions = this.getSession().getCurrentPage().getActionsThatCanBePerformed();
+        if (!allowedActions.contains(this.getFeature())) {
+            if (this.getSession().getCurrentUser() != null) {
+                this.getSession().getOutput().add(OutputHelper.error(this.getSession()));
+                return;
+            }
+            this.getSession().getOutput().add(OutputHelper.error(this.getSession()));
+            this.getSession().setCurrentPage(PageFactory.createPage("homepageUnauthenticated"));
+            this.getSession().setCurrentUser(null);
+            return;
+        }
 
+        for (Movie m : this.getSession().getCurrentUser().getPurchasedMovies()) {
+            if (m.getName().contains(this.getMovie())) {
+                this.getSession().getOutput().add(OutputHelper.error(this.getSession()));
+                return;
+            }
+        }
+
+        User user = this.getSession().getCurrentUser();
+        if (user.getCredentials().getAccountType().equals("standard")) {
+            int tokens = user.getTokensCount();
+            if (tokens < 2) {
+                this.getSession().getOutput().add(OutputHelper.error(this.getSession()));
+                return;
+            }
+
+            Movie movie = null;
+            for (Movie m : this.getSession().getDatabaseMovies()) {
+                if (m.getName().equals(this.getMovie())) {
+                    movie = m;
+                    break;
+                }
+            }
+            if (movie == null) {
+                this.getSession().getOutput().add(OutputHelper.error(this.getSession()));
+                return;
+            }
+
+            user.getPurchasedMovies().add(movie);
+            tokens -= 2;
+            user.setTokensCount(tokens);
+        } else {
+            Movie movie = null;
+            for (Movie m : this.getSession().getDatabaseMovies()) {
+                if (m.getName().equals(this.getMovie())) {
+                    movie = m;
+                    break;
+                }
+            }
+            if (movie == null) {
+                this.getSession().getOutput().add(OutputHelper.error(this.getSession()));
+                return;
+            }
+
+            if (user.getNumFreePremiumMovies() == 0) {
+                int tokens = user.getTokensCount();
+                if (tokens < 2) {
+                    this.getSession().getOutput().add(OutputHelper.error(this.getSession()));
+                    return;
+                }
+
+                tokens -= 2;
+                user.setTokensCount(tokens);
+            } else {
+                user.setNumFreePremiumMovies(user.getNumFreePremiumMovies() - 1);
+            }
+
+            user.getPurchasedMovies().add(movie);
+        }
     }
 }
--- Tema2/src/actions/Watch.java
+++ Tema2/src/actions/Watch.java
@@ -1,6 +1,12 @@
 package actions;
 
 import functionality.CurrentSession;
+import functionality.Movie;
+import functionality.OutputHelper;
+import functionality.User;
+import pages.PageFactory;
+
+import java.util.ArrayList;
 
 public class Watch extends Action {
     public Watch(CurrentSession session) {
@@ -8,6 +14,34 @@ public class Watch extends Action {
     }
 
     public void doAction() {
+        ArrayList<String> allowedActions = this.getSession().getCurrentPage().getActionsThatCanBePerformed();
+        if (!allowedActions.contains(this.getFeature())) {
+            if (this.getSession().getCurrentUser() != null) {
+                this.getSession().getOutput().add(OutputHelper.error(this.getSession()));
+                return;
+            }
+            this.getSession().getOutput().add(OutputHelper.error(this.getSession()));
+            this.getSession().setCurrentPage(PageFactory.createPage("homepageUnauthenticated"));
+            this.getSession().setCurrentUser(null);
+            return;
+        }
 
+        for (Movie m : this.getSession().getCurrentUser().getPurchasedMovies()) {
+            if (!m.getName().contains(this.getMovie())) {
+                this.getSession().getOutput().add(OutputHelper.error(this.getSession()));
+                return;
+            }
+        }
+
+        User user = this.getSession().getCurrentUser();
+        Movie movie = null;
+        for (Movie m : this.getSession().getDatabaseMovies()) {
+            if (m.getName().equals(this.getMovie())) {
+                movie = m;
+                break;
+            }
+        }
+
+        user.getWatchedMovies().add(movie);
     }
 }
